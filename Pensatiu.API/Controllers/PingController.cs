0e91b5ab34d42490b265ecef4c07964057ebd521
Fellipe Borges (licmsdn.13) <licmsdn.13@prosoft.com.br>
2019-01-18 14:50:34 -0200

    ping api creation

---
+++ Pensatiu.API/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pensatiu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(DateTime.UtcNow);
+        }
+    }
+}
