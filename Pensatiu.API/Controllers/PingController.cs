8b405440da314ef4a09fbde19e409a30fdc87fa9
Fellipe Borges (licmsdn.13) <licmsdn.13@prosoft.com.br>
2019-01-18 15:59:54 -0200

    Config test

--- Pensatiu.API/Controllers/PingController.cs
+++ Pensatiu.API/Controllers/PingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Pensatiu.API.Controllers
 {
@@ -10,10 +11,25 @@ namespace Pensatiu.API.Controllers
     [ApiController]
     public class PingController : Controller
     {
+        private IConfiguration _configuration;
+
+        public PingController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
             return Ok(DateTime.UtcNow);
+        }
+
+        [Route("config")]
+        [HttpGet]
+        public IActionResult Config()
+        {
+            var isCloudEnvironment = _configuration["IsCloudEnvironment"];
+            return Ok($"isCloudEnvironment: {isCloudEnvironment}");
         }
     }
 }
